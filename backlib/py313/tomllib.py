ed9cf9c60d68011aa6976c292a3a41feda3f986a
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-02 04:38:40 +0300

    feat: set the __module__

--- backlib/py313/tomllib.py
+++ backlib/py313/tomllib.py
@@ -2,3 +2,9 @@ from backlib.py313.internal.tomllib import TOMLDecodeError, load, loads
 
 
 __all__: list[str] = ["TOMLDecodeError", "load", "loads"]
+
+
+TOMLDecodeError.__module__ = __name__
+
+load.__module__ = __name__
+loads.__module__ = __name__
