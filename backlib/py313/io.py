f7d8e8e756bf6df253136ac0554f79e809e63a3f
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-03 01:12:19 +0300

    feat: add the stubs for planned modules

---
+++ backlib/py313/io.py
@@ -0,0 +1,5 @@
+__all__: list[str] = []
+
+
+_detail = "The 'backlib.py313.io' is being developed"
+raise NotImplementedError(_detail)
---
+++ backlib/py313/shutil.py
@@ -0,0 +1,5 @@
+__all__: list[str] = []
+
+
+_detail = "The 'backlib.py313.shutil' is being developed"
+raise NotImplementedError(_detail)
