f00eb3bce6ab8c6a2809fe237a62943c5e38c40a
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-02 18:39:50 +0300

    lint: ruff

--- backlib/py313/internal/json/__init__.py
+++ backlib/py313/internal/json/__init__.py
@@ -2,8 +2,14 @@ from __future__ import annotations
 
 import codecs
 
+from typing import TYPE_CHECKING, Any
+
 from backlib.py313.internal.json.decoder import JSONDecodeError, JSONDecoder
 from backlib.py313.internal.json.encoder import JSONEncoder
+
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 
 __author__ = "Bob Ippolito <bob@redivi.com>"
@@ -20,9 +26,21 @@ __all__: list[str] = [
 ]
 
 
-def dump(obj, fp, *, skipkeys=False, ensure_ascii=True, check_circular=True,
-        allow_nan=True, cls=None, indent=None, separators=None,
-        default=None, sort_keys=False, **kwargs):
+def dump(  # noqa: PLR0913
+    obj: Any,  # noqa: ANN401
+    fp,
+    *,
+    skipkeys: bool = False,
+    ensure_ascii: bool = True,
+    check_circular: bool = True,
+    allow_nan: bool = True,
+    cls: type[JSONEncoder] | None = None,
+    indent: int | str | None = None,
+    separators: tuple[str, str] | None = None,
+    default: Callable[[Any], Any] | None = None,
+    sort_keys: bool = False,
+    **kwargs: Any,  # noqa: ANN401
+) -> None:
     """Serialize `obj` as a JSON formatted stream to `fp`.
 
     See Also
@@ -52,9 +70,20 @@ def dump(obj, fp, *, skipkeys=False, ensure_ascii=True, check_circular=True,
         fp.write(chunk)
 
 
-def dumps(obj, *, skipkeys=False, ensure_ascii=True, check_circular=True,
-        allow_nan=True, cls=None, indent=None, separators=None,
-        default=None, sort_keys=False, **kwargs):
+def dumps(  # noqa: PLR0913
+    obj: Any,  # noqa: ANN401
+    *,
+    skipkeys: bool = False,
+    ensure_ascii: bool = True,
+    check_circular: bool = True,
+    allow_nan: bool = True,
+    cls: type[JSONEncoder] | None = None,
+    indent: int | str | None = None,
+    separators: tuple[str, str] | None = None,
+    default: Callable[[Any], Any] | None = None,
+    sort_keys: bool = False,
+    **kwargs: Any,  # noqa: ANN401
+) -> str:
     """Serialize `obj` to a JSON formatted `str`.
 
     See Also
@@ -83,16 +112,17 @@ def dumps(obj, *, skipkeys=False, ensure_ascii=True, check_circular=True,
     return encoder.encode(obj)
 
 
-def detect_encoding(b: bytes | bytearray):
-    bstartswith = b.startswith
-    if bstartswith((codecs.BOM_UTF32_BE, codecs.BOM_UTF32_LE)):
+def detect_encoding(b: bytes | bytearray) -> str:  # noqa: PLR0911
+    if b.startswith((codecs.BOM_UTF32_BE, codecs.BOM_UTF32_LE)):
         return "utf-32"
-    if bstartswith((codecs.BOM_UTF16_BE, codecs.BOM_UTF16_LE)):
+
+    if b.startswith((codecs.BOM_UTF16_BE, codecs.BOM_UTF16_LE)):
         return "utf-16"
-    if bstartswith(codecs.BOM_UTF8):
+
+    if b.startswith(codecs.BOM_UTF8):
         return "utf-8-sig"
 
-    if len(b) >= 4:
+    if len(b) >= 4:  # noqa: PLR2004
         if not b[0]:
             # 00 00 -- -- - utf-32-be
             # 00 XX -- -- - utf-16-be
@@ -102,7 +132,7 @@ def detect_encoding(b: bytes | bytearray):
             # XX 00 00 XX - utf-16-le
             # XX 00 XX -- - utf-16-le
             return "utf-16-le" if b[2] or b[3] else "utf-32-le"
-    elif len(b) == 2:
+    elif len(b) == 2:  # noqa: PLR2004
         if not b[0]:
             # 00 XX - utf-16-be
             return "utf-16-be"
@@ -113,8 +143,17 @@ def detect_encoding(b: bytes | bytearray):
     return "utf-8"
 
 
-def load(fp, *, cls=None, object_hook=None, parse_float=None,
-        parse_int=None, parse_constant=None, object_pairs_hook=None, **kwargs):
+def load(
+    fp,
+    *,
+    cls: type[JSONDecoder] | None = None,
+    object_hook: Callable[[dict[Any, Any]], Any] | None = None,
+    parse_float: Callable[[str], Any] | None = None,
+    parse_int: Callable[[str], Any] | None = None,
+    parse_constant: Callable[[str], Any] | None = None,
+    object_pairs_hook: Callable[[list[tuple[Any, Any]]], Any] | None = None,
+    **kwargs: Any,  # noqa: ANN401
+) -> Any:  # noqa: ANN401
     """Deserialize `fp` to a Python object.
 
     See Also
@@ -137,8 +176,17 @@ def load(fp, *, cls=None, object_hook=None, parse_float=None,
     )
 
 
-def loads(s, *, cls=None, object_hook=None, parse_float=None,
-        parse_int=None, parse_constant=None, object_pairs_hook=None, **kwargs):
+def loads(
+    s: str | bytes | bytearray,
+    *,
+    cls: type[JSONDecoder] | None = None,
+    object_hook: Callable[[dict[Any, Any]], Any] | None = None,
+    parse_float: Callable[[str], Any] | None = None,
+    parse_int: Callable[[str], Any] | None = None,
+    parse_constant: Callable[[str], Any] | None = None,
+    object_pairs_hook: Callable[[list[tuple[Any, Any]]], Any] | None = None,
+    **kwargs: Any,  # noqa: ANN401
+) -> Any:  # noqa: ANN401
     """Deserialize `s` to a Python object.
 
     See Also
@@ -160,21 +208,22 @@ def loads(s, *, cls=None, object_hook=None, parse_float=None,
 
         s = s.decode(detect_encoding(s), "surrogatepass")
 
-    if (cls is None and object_hook is None and
-            parse_int is None and parse_float is None and
-            parse_constant is None and object_pairs_hook is None and not kwargs):
-        decoder = JSONDecoder(object_hook=None, object_pairs_hook=None)
-        return decoder.decode(s)
     if cls is None:
         cls = JSONDecoder
+
     if object_hook is not None:
         kwargs["object_hook"] = object_hook
+
     if object_pairs_hook is not None:
         kwargs["object_pairs_hook"] = object_pairs_hook
+
     if parse_float is not None:
         kwargs["parse_float"] = parse_float
+
     if parse_int is not None:
         kwargs["parse_int"] = parse_int
+
     if parse_constant is not None:
         kwargs["parse_constant"] = parse_constant
+
     return cls(**kwargs).decode(s)
