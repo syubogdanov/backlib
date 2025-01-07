dccaecb3c6a0d5c07d7c37b9a67fde545c919e6b
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-07 15:24:53 +0300

    feat: __all__

--- backlib/internal/stdlib/py313/ospath/src/posixpath.py
+++ backlib/internal/stdlib/py313/ospath/src/posixpath.py
@@ -2,6 +2,30 @@ from backlib.internal.stdlib.py313.os import fspath
 from backlib.internal.stdlib.py313.ospath.src.typing import StrOrBytesPath
 
 
+__all__: list[str] = [
+    "abspath",
+    "basename",
+    "commonpath",
+    "dirname",
+    "expanduser",
+    "expandvars",
+    "isabs",
+    "isdevdrive",
+    "isjunction",
+    "ismount",
+    "join",
+    "normcase",
+    "normpath",
+    "realpath",
+    "relpath",
+    "split",
+    "splitdrive",
+    "splitext",
+    "splitroot",
+    "supports_unicode_filenames",
+]
+
+
 def isjunction(path: StrOrBytesPath) -> bool:
     """Return `True` if `path` refers to an existing directory entry that is a junction.
 
