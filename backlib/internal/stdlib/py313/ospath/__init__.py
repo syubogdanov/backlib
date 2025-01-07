d5d764cefad7abbd884fdffe410e0762ad0a3124
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-07 14:56:52 +0300

    feat: set __module__

--- backlib/internal/stdlib/py313/ospath/__init__.py
+++ backlib/internal/stdlib/py313/ospath/__init__.py
@@ -104,3 +104,42 @@ __all__: list[str] = [
     "splitroot",
     "supports_unicode_filenames",
 ]
+
+__backlib__: str = "backlib.py313.os.path"
+
+
+abspath.__module__ = __backlib__
+basename.__module__ = __backlib__
+commonpath.__module__ = __backlib__
+commonprefix.__module__ = __backlib__
+dirname.__module__ = __backlib__
+exists.__module__ = __backlib__
+expanduser.__module__ = __backlib__
+expandvars.__module__ = __backlib__
+getatime.__module__ = __backlib__
+getctime.__module__ = __backlib__
+getmtime.__module__ = __backlib__
+getsize.__module__ = __backlib__
+isabs.__module__ = __backlib__
+isdevdrive.__module__ = __backlib__
+isdir.__module__ = __backlib__
+isfile.__module__ = __backlib__
+isjunction.__module__ = __backlib__
+islink.__module__ = __backlib__
+ismount.__module__ = __backlib__
+join.__module__ = __backlib__
+lexists.__module__ = __backlib__
+normcase.__module__ = __backlib__
+normpath.__module__ = __backlib__
+realpath.__module__ = __backlib__
+relpath.__module__ = __backlib__
+samefile.__module__ = __backlib__
+sameopenfile.__module__ = __backlib__
+samestat.__module__ = __backlib__
+split.__module__ = __backlib__
+splitdrive.__module__ = __backlib__
+splitext.__module__ = __backlib__
+splitroot.__module__ = __backlib__
+
+# NOTE: 'bool' object has no attribute '__module__'
+# supports_unicode_filenames.__module__ = __backlib__  # noqa: ERA001
