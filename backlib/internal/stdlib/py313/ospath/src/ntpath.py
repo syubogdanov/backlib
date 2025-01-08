36b81f615060dac5e5b138c9ec147bc1155da189
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-08 16:30:52 +0300

    feat: normpath

--- backlib/internal/stdlib/py313/ospath/src/ntpath.py
+++ backlib/internal/stdlib/py313/ospath/src/ntpath.py
@@ -424,3 +424,50 @@ def expandvars(path: AnyStr | PathLike[AnyStr]) -> AnyStr:  # noqa: C901, PLR091
             res += c
         index += 1
     return res
+
+
+@techdebt("This function should be refactored...")
+def normpath(path: AnyStr | PathLike[AnyStr]) -> AnyStr:
+    """Normalize a pathname by collapsing redundant separators and up-level references.
+
+    See Also
+    --------
+    * `os.path.normpath`.
+
+    Version
+    -------
+    * Python 3.13.
+    """
+    path = fspath(path)
+    if isinstance(path, bytes):
+        sep = b"\\"
+        altsep = b"/"
+        curdir = b"."
+        pardir = b".."
+    else:
+        sep = "\\"
+        altsep = "/"
+        curdir = "."
+        pardir = ".."
+    path = path.replace(altsep, sep)
+    drive, root, path = splitroot(path)
+    prefix = drive + root
+    comps = path.split(sep)
+    i = 0
+    while i < len(comps):
+        if not comps[i] or comps[i] == curdir:
+            del comps[i]
+        elif comps[i] == pardir:
+            if i > 0 and comps[i-1] != pardir:
+                del comps[i-1:i+1]
+                i -= 1
+            elif i == 0 and root:
+                del comps[i]
+            else:
+                i += 1
+        else:
+            i += 1
+    # If the path is now empty, substitute '.'
+    if not prefix and not comps:
+        comps.append(curdir)
+    return prefix + sep.join(comps)
