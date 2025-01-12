9b6636d0674c2efd48a3516588a3c2f70f1ea4ea
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-12 19:39:42 +0300

    feat: docstring, __all__

--- backlib/internal/stdlib/py313/os/src/__init__.py
+++ backlib/internal/stdlib/py313/os/src/__init__.py
@@ -1,3 +1,318 @@
+"""Backport of the `os` module.
+
+Notes
+-----
+* The following constants are not backported:
+    - `CLD_CONTINUED`;
+    - `CLD_DUMPED`;
+    - `CLD_EXITED`;
+    - `CLD_KILLED`;
+    - `CLD_STOPPED`;
+    - `CLD_TRAPPED`;
+    - `CLONE_FILES`;
+    - `CLONE_FS`;
+    - `CLONE_NEWCGROUP`;
+    - `CLONE_NEWIPC`;
+    - `CLONE_NEWNET`;
+    - `CLONE_NEWNS`;
+    - `CLONE_NEWPID`;
+    - `CLONE_NEWTIME`;
+    - `CLONE_NEWUSER`;
+    - `CLONE_NEWUTS`;
+    - `CLONE_SIGHAND`;
+    - `CLONE_SYSVSEM`;
+    - `CLONE_THREAD`;
+    - `CLONE_VM`;
+    - `EFD_CLOEXEC`;
+    - `EFD_NONBLOCK`;
+    - `EFD_SEMAPHORE`;
+    - `EX_CANTCREAT`;
+    - `EX_CONFIG`;
+    - `EX_DATAERR`;
+    - `EX_IOERR`;
+    - `EX_NOHOST`;
+    - `EX_NOINPUT`;
+    - `EX_NOPERM`;
+    - `EX_NOUSER`;
+    - `EX_OSERR`;
+    - `EX_OSFILE`;
+    - `EX_PROTOCOL`;
+    - `EX_SOFTWARE`;
+    - `EX_TEMPFAIL`;
+    - `EX_UNAVAILABLE`;
+    - `EX_USAGE`;
+    - `F_LOCK`;
+    - `F_TEST`;
+    - `F_TLOCK`;
+    - `F_ULOCK`;
+    - `GRND_NONBLOCK`;
+    - `GRND_RANDOM`;
+    - `MFD_ALLOW_SEALING`;
+    - `MFD_CLOEXEC`;
+    - `MFD_HUGETLB`;
+    - `MFD_HUGE_16GB`;
+    - `MFD_HUGE_16MB`;
+    - `MFD_HUGE_1GB`;
+    - `MFD_HUGE_1MB`;
+    - `MFD_HUGE_256MB`;
+    - `MFD_HUGE_2GB`;
+    - `MFD_HUGE_2MB`;
+    - `MFD_HUGE_32MB`;
+    - `MFD_HUGE_512KB`;
+    - `MFD_HUGE_512MB`;
+    - `MFD_HUGE_64KB`;
+    - `MFD_HUGE_8MB`;
+    - `MFD_HUGE_MASK`;
+    - `MFD_HUGE_SHIFT`;
+    - `NGROUPS_MAX`;
+    - `O_ACCMODE`;
+    - `O_ASYNC`;
+    - `O_BINARY`;
+    - `O_CLOEXEC`;
+    - `O_DIRECTORY`;
+    - `O_DIRECT`;
+    - `O_DSYNC`;
+    - `O_EVTONLY`;
+    - `O_EXEC`;
+    - `O_EXLOCK`;
+    - `O_FSYNC`;
+    - `O_LARGEFILE`;
+    - `O_NDELAY`;
+    - `O_NOATIME`;
+    - `O_NOCTTY`;
+    - `O_NOFOLLOW_ANY`;
+    - `O_NOFOLLOW`;
+    - `O_NOINHERIT`;
+    - `O_NONBLOCK`;
+    - `O_PATH`;
+    - `O_RANDOM`;
+    - `O_RSYNC`;
+    - `O_SEARCH`;
+    - `O_SEQUENTIAL`;
+    - `O_SHLOCK`;
+    - `O_SHORT_LIVED`;
+    - `O_SYMLINK`;
+    - `O_SYNC`;
+    - `O_TEMPORARY`;
+    - `O_TEXT`;
+    - `O_TMPFILE`;
+    - `POSIX_FADV_DONTNEED`;
+    - `POSIX_FADV_NOREUSE`;
+    - `POSIX_FADV_NORMAL`;
+    - `POSIX_FADV_RANDOM`;
+    - `POSIX_FADV_SEQUENTIAL`;
+    - `POSIX_FADV_WILLNEED`;
+    - `POSIX_SPAWN_CLOSEFROM`;
+    - `POSIX_SPAWN_CLOSE`;
+    - `POSIX_SPAWN_DUP2`;
+    - `POSIX_SPAWN_OPEN`;
+    - `PRIO_DARWIN_BG`;
+    - `PRIO_DARWIN_NONUI`;
+    - `PRIO_DARWIN_PROCESS`;
+    - `PRIO_DARWIN_THREAD`;
+    - `PRIO_PGRP`;
+    - `PRIO_PROCESS`;
+    - `PRIO_USER`;
+    - `P_ALL`;
+    - `P_DETACH`;
+    - `P_OVERLAY`;
+    - `P_PGID`;
+    - `P_PIDFD`;
+    - `P_PID`;
+    - `RTLD_DEEPBIND`;
+    - `RTLD_GLOBAL`;
+    - `RTLD_LAZY`;
+    - `RTLD_LOCAL`;
+    - `RTLD_NODELETE`;
+    - `RTLD_NOLOAD`;
+    - `RTLD_NOW`;
+    - `RWF_APPEND`;
+    - `RWF_DSYNC`;
+    - `RWF_HIPRI`;
+    - `RWF_NOWAIT`;
+    - `RWF_SYNC`;
+    - `SCHED_BATCH`;
+    - `SCHED_FIFO`;
+    - `SCHED_IDLE`;
+    - `SCHED_OTHER`;
+    - `SCHED_RESET_ON_FORK`;
+    - `SCHED_RR`;
+    - `SEEK_DATA`;
+    - `SEEK_HOLE`;
+    - `SPLICE_F_MORE`;
+    - `SPLICE_F_MOVE`;
+    - `SPLICE_F_NONBLOCK`;
+    - `ST_APPEND`;
+    - `ST_MANDLOCK`;
+    - `ST_NOATIME`;
+    - `ST_NODEV`;
+    - `ST_NODIRATIME`;
+    - `ST_NOEXEC`;
+    - `ST_NOSUID`;
+    - `ST_RDONLY`;
+    - `ST_RELATIME`;
+    - `ST_SYNCHRONOUS`;
+    - `ST_WRITE`;
+    - `TFD_CLOEXEC`;
+    - `TFD_NONBLOCK`;
+    - `TFD_TIMER_ABSTIME`;
+    - `TFD_TIMER_CANCEL_ON_SET`;
+    - `WCONTINUED`;
+    - `WCOREDUMP`;
+    - `WEXITED`;
+    - `WEXITSTATUS`;
+    - `WIFCONTINUED`;
+    - `WIFEXITED`;
+    - `WIFSIGNALED`;
+    - `WIFSTOPPED`;
+    - `WNOHANG`;
+    - `WNOWAIT`;
+    - `WSTOPPED`;
+    - `WSTOPSIG`;
+    - `WTERMSIG`;
+    - `WUNTRACED`;
+    - `XATTR_CREATE`;
+    - `XATTR_REPLACE`;
+    - `XATTR_SIZE_MAX`.
+* The following functions are not backported:
+    - `chflags`;
+    - `chown`;
+    - `chroot`;
+    - `confstr_names`;
+    - `confstr`;
+    - `copy_file_range`;
+    - `ctermid`;
+    - `environb`;
+    - `eventfd_read`;
+    - `eventfd_write`;
+    - `eventfd`;
+    - `fchdir`;
+    - `fchown`;
+    - `fdatasync`;
+    - `fork`;
+    - `forkpty`;
+    - `fpathconf`;
+    - `fstatvfs`;
+    - `fwalk`;
+    - `get_handle_inheritable`;
+    - `getegid`;
+    - `getenvb`;
+    - `geteuid`;
+    - `getgid`;
+    - `getgrouplist`;
+    - `getgroups`;
+    - `getloadavg`;
+    - `getpgid`;
+    - `getpgrp`;
+    - `getpriority`;
+    - `getrandom`;
+    - `getresgid`;
+    - `getresuid`;
+    - `getsid`;
+    - `getuid`;
+    - `getxattr`;
+    - `grantpt`;
+    - `initgroups`;
+    - `killpg`;
+    - `lchflags`;
+    - `lchmod`;
+    - `lchown`;
+    - `listdrives`;
+    - `listmounts`;
+    - `listvolumes`;
+    - `listxattr`;
+    - `lockf`;
+    - `login_tty`;
+    - `major`;
+    - `makedev`;
+    - `memfd_create`;
+    - `minor`;
+    - `mkfifo`;
+    - `mknod`;
+    - `nice`;
+    - `openpty`;
+    - `pathconf_names`;
+    - `pathconf`;
+    - `pidfd_open`;
+    - `pipe2`;
+    - `posix_fadvise`;
+    - `posix_fallocate`;
+    - `posix_openpt`;
+    - `posix_spawn`;
+    - `posix_spawnp`;
+    - `pread`;
+    - `preadv`;
+    - `ptsname`;
+    - `pwrite`;
+    - `pwritev`;
+    - `readv`;
+    - `register_at_fork`;
+    - `removexattr`;
+    - `sched_get_priority_max`;
+    - `sched_get_priority_min`;
+    - `sched_getaffinity`;
+    - `sched_getparam`;
+    - `sched_getscheduler`;
+    - `sched_param`;
+    - `sched_rr_get_interval`;
+    - `sched_setaffinity`;
+    - `sched_setparam`;
+    - `sched_setscheduler`;
+    - `sched_yield`;
+    - `sendfile`;
+    - `set_handle_inheritable`;
+    - `setegid`;
+    - `seteuid`;
+    - `setgid`;
+    - `setgroups`;
+    - `setns`;
+    - `setpgid`;
+    - `setpgrp`;
+    - `setpriority`;
+    - `setregid`;
+    - `setresgid`;
+    - `setresuid`;
+    - `setreuid`;
+    - `setsid`;
+    - `setuid`;
+    - `setxattr`;
+    - `spawnlp`;
+    - `spawnlpe`;
+    - `spawnvp`;
+    - `spawnvpe`;
+    - `splice`;
+    - `startfile`;
+    - `statvfs`;
+    - `sync`;
+    - `sysconf_names`;
+    - `sysconf`;
+    - `tcgetpgrp`;
+    - `tcsetpgrp`;
+    - `timerfd_create`;
+    - `timerfd_gettime_ns`;
+    - `timerfd_gettime`;
+    - `timerfd_settime_ns`;
+    - `timerfd_settime`;
+    - `ttyname`;
+    - `uname`;
+    - `unlockpt`;
+    - `unshare`;
+    - `wait3`;
+    - `wait4`;
+    - `wait`;
+    - `waitid_result`;
+    - `waitid`;
+    - `writev`.
+
+See Also
+--------
+* `os`.
+
+Version
+-------
+* Python 3.13.
+"""
+
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
@@ -13,6 +328,137 @@ from os import stat_result as stat_result
 from typing import Generic, TypeVar
 
 from backlib.internal.typing import AnyStr, Self
+
+
+__all__: list[str] = [
+    "EX_OK",
+    "F_OK",
+    "O_APPEND",
+    "O_CREAT",
+    "O_EXCL",
+    "O_RDONLY",
+    "O_RDWR",
+    "O_TRUNC",
+    "O_WRONLY",
+    "P_NOWAIT",
+    "P_NOWAITO",
+    "P_WAIT",
+    "R_OK",
+    "SEEK_CUR",
+    "SEEK_END",
+    "SEEK_SET",
+    "TMP_MAX",
+    "W_OK",
+    "X_OK",
+    "DirEntry",
+    "PathLike",
+    "_exit",
+    "abort",
+    "access",
+    "altsep",
+    "chdir",
+    "chmod",
+    "close",
+    "closerange",
+    "cpu_count",
+    "curdir",
+    "defpath",
+    "device_encoding",
+    "devnull",
+    "dup",
+    "dup2",
+    "environ",
+    "environb",
+    "error",
+    "errorEX_OK",
+    "execl",
+    "execle",
+    "execlp",
+    "execlpe",
+    "execv",
+    "execve",
+    "execvp",
+    "execvpe",
+    "extsep",
+    "fchmod",
+    "fdopen",
+    "fsdecode",
+    "fsencode",
+    "fspath",
+    "fstat",
+    "fsync",
+    "ftruncate",
+    "get_blocking",
+    "get_exec_path",
+    "get_inheritable",
+    "get_terminal_size",
+    "getcwd",
+    "getcwdb",
+    "getenv",
+    "getenvb",
+    "getlogin",
+    "getpid",
+    "getppid",
+    "isatty",
+    "kill",
+    "linesep",
+    "link",
+    "listdir",
+    "lseek",
+    "lstat",
+    "makedirs",
+    "mkdir",
+    "name",
+    "open",
+    "pardir",
+    "path",
+    "pathsep",
+    "pipe",
+    "popen",
+    "process_cpu_count",
+    "putenv",
+    "read",
+    "readlink",
+    "remove",
+    "removedirs",
+    "rename",
+    "renames",
+    "replace",
+    "rmdir",
+    "scandir",
+    "sep",
+    "set_blocking",
+    "set_inheritable",
+    "spawnl",
+    "spawnle",
+    "spawnv",
+    "spawnve",
+    "stat",
+    "stat_result",
+    "statvfs_result",
+    "strerror",
+    "supports_bytes_environ",
+    "supports_dir_fd",
+    "supports_effective_ids",
+    "supports_fd",
+    "supports_follow_symlinks",
+    "symlink",
+    "system",
+    "terminal_size",
+    "times",
+    "times_result",
+    "truncate",
+    "umask",
+    "uname_result",
+    "unlink",
+    "unsetenv",
+    "urandom",
+    "utime",
+    "waitpid",
+    "waitstatus_to_exitcode",
+    "walk",
+    "write",
+]
 
 
 AnyStr_co = TypeVar("AnyStr_co", str, bytes, covariant=True)
