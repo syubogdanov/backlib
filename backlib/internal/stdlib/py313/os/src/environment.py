bf8cdc9fb98a7f64ecc92b54856ef28594bface2
Sergey Bogdanov <se.y.bogdanov@tinkoff.ru>
2025-02-15 15:08:21 +0300

    fix

--- backlib/internal/stdlib/py313/os/src/environment.py
+++ backlib/internal/stdlib/py313/os/src/environment.py
@@ -3,10 +3,15 @@ from __future__ import annotations
 import os as py_os
 
 from backlib.internal.utils import alias
-from backlib.internal.utils.platform import is_nt
+from backlib.internal.utils.platform import is_nt, is_posix
 
 
 __all__: list[str] = ["environ", "environb", "supports_bytes_environ"]
+
+
+if not is_nt() and not is_posix():
+    detail = "no os specific module found"
+    raise ImportError(detail)
 
 
 supports_bytes_environ = not is_nt()
