56366e0fcdf7ed8ebfc7100e98385278cc005c69
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-02-16 20:42:36 +0300

    feat: type hints

--- backlib/internal/stdlib/py313/io/src/openers.py
+++ backlib/internal/stdlib/py313/io/src/openers.py
@@ -1,9 +1,142 @@
-from typing import BinaryIO
+from __future__ import annotations
+
+from typing import IO, TYPE_CHECKING, BinaryIO, Literal, overload
 from warnings import warn
 
+from backlib.internal.typing import AnyStr
+from backlib.internal.utils.typing import (
+    OpenBinaryMode,
+    OpenBinaryModeReading,
+    OpenBinaryModeUpdating,
+    OpenBinaryModeWriting,
+    OpenTextMode,
+)
 
-def open():
-    ...
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from backlib.internal.stdlib.py313.io.src.classes import (
+        BufferedRandom,
+        BufferedReader,
+        BufferedWriter,
+        FileIO,
+        TextIOWrapper,
+    )
+    from backlib.internal.stdlib.py313.os import PathLike
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: OpenTextMode = "r",
+    buffering: int = -1,
+    encoding: str | None = None,
+    errors: str | None = None,
+    newline: str | None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> TextIOWrapper: ...
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: OpenBinaryMode,
+    buffering: Literal[0],
+    encoding: None = None,
+    errors: None = None,
+    newline: None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> FileIO: ...
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: OpenBinaryModeUpdating,
+    buffering: Literal[-1, 1] = -1,
+    encoding: None = None,
+    errors: None = None,
+    newline: None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> BufferedRandom: ...
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: OpenBinaryModeWriting,
+    buffering: Literal[-1, 1] = -1,
+    encoding: None = None,
+    errors: None = None,
+    newline: None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> BufferedWriter: ...
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: OpenBinaryModeReading,
+    buffering: Literal[-1, 1] = -1,
+    encoding: None = None,
+    errors: None = None,
+    newline: None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> BufferedReader: ...
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: OpenBinaryMode,
+    buffering: int = -1,
+    encoding: None = None,
+    errors: None = None,
+    newline: None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> BinaryIO: ...
+
+
+@overload
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: str,
+    buffering: int = -1,
+    encoding: str | None = None,
+    errors: str | None = None,
+    newline: str | None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> IO[AnyStr]: ...
+
+
+def open(  # noqa: A001
+    file: int | AnyStr | PathLike[AnyStr],
+    mode: str = "r",
+    buffering: int = -1,
+    encoding: str | None = None,
+    errors: str | None = None,
+    newline: str | None = None,
+    closefd: bool = True,  # noqa: FBT001, FBT002
+    opener: Callable[[str, int], int] | None = None,
+) -> IO[AnyStr]:
+    """Open `file` and return a corresponding file object.
+
+    See Also
+    --------
+    * `io.open`.
+
+    Version
+    -------
+    * Python 3.13.
+    """
 
 
 def open_code(path: str) -> BinaryIO:
--- backlib/internal/utils/typing.py
+++ backlib/internal/utils/typing.py
@@ -1,10 +1,18 @@
-from typing import Protocol, TypeVar
+from typing import Literal, Protocol, TypeVar
 
 from backlib.internal.collections import Buffer
 from backlib.internal.typing import Self, TypeAlias
 
 
 __all__: list[str] = [
+    "OpenBinaryMode",
+    "OpenBinaryModeReading",
+    "OpenBinaryModeUpdating",
+    "OpenBinaryModeWriting",
+    "OpenTextMode",
+    "OpenTextModeReading",
+    "OpenTextModeUpdating",
+    "OpenTextModeWriting",
     "ReadOnlyBuffer",
     "ReadableBuffer",
     "SupportsRead",
@@ -15,6 +23,88 @@ __all__: list[str] = [
 
 T_co = TypeVar("T_co", covariant=True)
 T_contra = TypeVar("T_contra", contravariant=True)
+
+
+OpenTextModeUpdating: TypeAlias = Literal[
+    "r+",
+    "+r",
+    "rt+",
+    "r+t",
+    "+rt",
+    "tr+",
+    "t+r",
+    "+tr",
+    "w+",
+    "+w",
+    "wt+",
+    "w+t",
+    "+wt",
+    "tw+",
+    "t+w",
+    "+tw",
+    "a+",
+    "+a",
+    "at+",
+    "a+t",
+    "+at",
+    "ta+",
+    "t+a",
+    "+ta",
+    "x+",
+    "+x",
+    "xt+",
+    "x+t",
+    "+xt",
+    "tx+",
+    "t+x",
+    "+tx",
+]
+OpenTextModeWriting: TypeAlias = Literal["w", "wt", "tw", "a", "at", "ta", "x", "xt", "tx"]
+OpenTextModeReading: TypeAlias = Literal[
+    "r",
+    "rt",
+    "tr",
+    "U",
+    "rU",
+    "Ur",
+    "rtU",
+    "rUt",
+    "Urt",
+    "trU",
+    "tUr",
+    "Utr",
+]
+OpenTextMode: TypeAlias = OpenTextModeUpdating | OpenTextModeWriting | OpenTextModeReading
+
+OpenBinaryModeUpdating: TypeAlias = Literal[
+    "rb+",
+    "r+b",
+    "+rb",
+    "br+",
+    "b+r",
+    "+br",
+    "wb+",
+    "w+b",
+    "+wb",
+    "bw+",
+    "b+w",
+    "+bw",
+    "ab+",
+    "a+b",
+    "+ab",
+    "ba+",
+    "b+a",
+    "+ba",
+    "xb+",
+    "x+b",
+    "+xb",
+    "bx+",
+    "b+x",
+    "+bx",
+]
+OpenBinaryModeWriting: TypeAlias = Literal["wb", "bw", "ab", "ba", "xb", "bx"]
+OpenBinaryModeReading: TypeAlias = Literal["rb", "br", "rbU", "rUb", "Urb", "brU", "bUr", "Ubr"]
+OpenBinaryMode: TypeAlias = OpenBinaryModeUpdating | OpenBinaryModeReading | OpenBinaryModeWriting
 
 
 ReadableBuffer: TypeAlias = Buffer
