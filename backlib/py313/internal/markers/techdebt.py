cfa230a3d8de402500be9ec8692d849b2bd7671e
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-03-11 02:37:26 +0300

    chore: remove the unused

--- backlib/py313/internal/markers/techdebt.py
+++ backlib/py313/internal/markers/techdebt.py
@@ -4,21 +4,6 @@ from typing import TypeVar
 T = TypeVar("T")
 
 
-def platform(debt: T) -> T:
-    """Mark the object as platform-specific.
-
-    Notes
-    -----
-    * Platform-specific objects are not portable.
-    """
-    return debt
-
-
-def refactor(debt: T) -> T:
-    """Mark the object as needing refactoring."""
-    return debt
-
-
 def simplified(debt: T) -> T:
     """Mark the object as simplified.
 
