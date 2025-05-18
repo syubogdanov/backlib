943423052dfbdab3a7e593976192562414f92eaf
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-05-18 22:23:06 +0300

    fix: bug

--- backlib/internal/backports/py313/os_path/__init__.py
+++ backlib/internal/backports/py313/os_path/__init__.py
@@ -33,16 +33,19 @@ from backlib.internal.backports.py312.os_path import (
     splitroot,
     supports_unicode_filenames,
 )
+from backlib.internal.utils.platform import is_nt
 
 
 if sys.version_info >= (3, 13):
-    from os.path import commonpath, isabs, isreserved
+    from os.path import commonpath, isabs
 else:
-    from backlib.internal.backports.py313.os_path.internal.os_path import (
-        commonpath,
-        isabs,
-        isreserved,
-    )
+    from backlib.internal.backports.py313.os_path.internal.os_path import commonpath, isabs
+
+
+if sys.version_info >= (3, 13) and is_nt():
+    from os.path import isreserved
+else:
+    from backlib.internal.backports.py313.os_path.internal.os_path import isreserved
 
 
 __all__: list[str] = [
